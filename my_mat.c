4da1b20a8d64fd466e7d412865fdd74347713783
Segev955 <Segev95@gmail.comgit config --global user.email Segev95@gmail.com>
2021-11-22 20:54:14 +0200

    fix

--- my_mat.c
+++ my_mat.c
@@ -5,22 +5,22 @@
 #define false 0
 #define num 10
 
-int fun1(int a[][]) { //if a
-	for (int i = 1; i<=num; i++) {
-		for (int j = 1; j<=num; j++) {
+void fun1(int a[num][num]) { //if a
+	
+	for (int i = 0; i<num; i++) {
+		for (int j = 0; j<num; j++) {
 			scanf ("%d", &a[i][j]);
 		}
 	}
-	return a;
 }
-int fun2(int i, int j,int a[][]) { //if b
+int fun2(int i, int j,int a[num][num]) { //if b
 	if (algo(a,i,j) > 0) {
 		return true;
 	}
 	return false;
 }
 
-int fun3(int i, int j,int a[][]) { //if c
+int fun3(int i, int j,int a[num][num]) { //if c
 	if (algo(a,i,j) > 0) {
 		return algo(a,i,j);
 	}
@@ -28,11 +28,11 @@ int fun3(int i, int j,int a[][]) { //if c
 }
 
 
-int algo (int a[][],int b,int c) {
+int algo (int a[num][num],int b,int c) {
 	//int arr[10][10] = a;
-	for (int k = 1; k<=num; k++) {
-		for (int i = 1; i<=num; i++) {
-			for (int j = 1; j<=num; j++) {
+	for (int k = 1; k<num; k++) {
+		for (int i = 1; i<num; i++) {
+			for (int j = 1; j<num; j++) {
 				int x = a[i][k]+a[k][j];
 				if (x == 0) {
 					continue;
