9f3fc9468d1d0a1302877455e0112e0f9c12a4ce
Segev955 <Segev95@gmail.comgit config --global user.email Segev95@gmail.com>
2021-11-22 20:53:57 +0200

    fix

--- main.c
+++ main.c
@@ -5,32 +5,38 @@
 #define num 10
 
 int main() {
-	char x;
+	char x = 'a';
 	int a[num][num];
 	int i,j;
+	int ans;
 	while (x != 'D') {
 		scanf("%c", &x);
 		if (x == 'A') {
-			a = fun1(a);
+			fun1(a);
 		}
 		if (x == 'B') {
 			scanf("%d %d", &i, &j);
 			if (fun2(i,j,a) == true) {
 				printf ("True");
+				printf("\n");
 			}
 			else {
 				printf ("False");
+				printf("\n");
 			}
 		}
 		if (x == 'C') {
 			scanf("%d %d", &i, &j);
 			ans = fun3(i,j,a);
-			if (fun3(i,j) == -1) {
+			if (fun3(i,j,a) == -1) {
 				printf ("-1");
+				printf("\n");
 			}
 			else {
-				print ("%d", ans);
+				printf ("%d", ans);
+				printf("\n");
 			}
 		}
 	}
+	printf("\n");
 }
