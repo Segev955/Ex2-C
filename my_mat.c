b6c5032e9c60fa3b429b0a4fef9e6fbb77eff003
dani4655 <dani4655@gmail.com>
2021-11-23 20:17:12 +0200

    notes

--- my_mat.c
+++ my_mat.c
@@ -4,7 +4,9 @@
 #define true 1
 #define false 0
 #define num 10
+#define infinity 0
 
+//this function gets a matrix and fill it with input from the user
 void fun1(int a[num][num]) { //if a
 	
 	for (int i = 0; i<num; i++) {
@@ -13,13 +15,14 @@ void fun1(int a[num][num]) { //if a
 		}
 	}
 }
+//this function checks if there is a path from i to j
 int fun2(int i, int j,int a[num][num]) { //if b
 	if (algo(a,i,j) > 0) {
 		return true;
 	}
 	return false;
 }
-
+//this function returns the shortest path from i to j by using the function algo if there is no path the function returns -1
 int fun3(int i, int j,int a[num][num]) { //if c
 	if (algo(a,i,j) > 0) {
 		return algo(a,i,j);
@@ -27,7 +30,7 @@ int fun3(int i, int j,int a[num][num]) { //if c
 	return -1;
 }
 
-
+//this function calculate the distance in the graph according to the video that was recommended to solve this assigment https://www.youtube.com/watch?v=oNI0rf2P9gE
 int algo (int a[num][num],int b,int c) {
 	//int arr[10][10] = a;
 	for (int k = 0; k<num; k++) {
@@ -40,7 +43,7 @@ int algo (int a[num][num],int b,int c) {
 				if (x == 0 || i == j || i == k || j == k) {
 					continue;
 				}
-				if (a[i][j] == 0) {
+				if (a[i][j] == infinity) {
                         		a[i][j] = x;
                     		}
 				if (a[i][j] > x) {
